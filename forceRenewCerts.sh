d0f51814fceb4cd91807cd47c1ac9f97c43c6b52
Tomas Vanagas <tomasvanagas@ymail.com>
2025-11-17 16:53:07 +0200

    Generate dynamically Bookstack APP_KEY

--- forceRenewCerts.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-sudo docker-compose down hosting-control-caddy
-sudo find ./control-modules/control-caddy/caddy_data/caddy/certificates -type f -exec rm {} \;
-sudo docker-compose up -d --build hosting-control-caddy
--- runUpdateThisStack.sh
+++ runUpdateThisStack.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-######### HOSTING MODULES DATA DIRECTORIES AND FILES #########
+################## HOSTING MODULES DATA DIRECTORIES AND FILES ##########################
 # Control backend database
 mkdir -p _DATA/control-backend
 
@@ -14,7 +14,7 @@ sudo chown -R 1000:1000 _DATA/control-docs
 # Create users-caddy directories
 mkdir -p _DATA/users-caddy
 touch _DATA/users-caddy/Caddyfile
-##############################################################
+########################################################################################
 
 
 
@@ -24,6 +24,24 @@ mkdir -p SERVERS
 
 # Build student environment
 sudo docker build -t hosting-users-dind -f ./dind/Dockerfile ./dind
+
+
+
+
+
+######################### GENERATE AND STORE BOOKSTACK_APP_KEY #########################
+# Only generate if BOOKSTACK_APP_KEY doesn't exist in .env
+if [ ! -f .env ] || ! grep -q "^BOOKSTACK_APP_KEY=" .env; then
+    echo "Generating BOOKSTACK_APP_KEY..."
+    BOOKSTACK_APP_KEY="base64:$(openssl rand -base64 32)"
+    
+    echo "BOOKSTACK_APP_KEY=$BOOKSTACK_APP_KEY" >> .env
+    echo "BOOKSTACK_APP_KEY added to .env"
+    echo "BOOKSTACK_APP_KEY: $BOOKSTACK_APP_KEY"
+fi
+#########################################################################################
+
+
 
 
 # Create network and start containers
