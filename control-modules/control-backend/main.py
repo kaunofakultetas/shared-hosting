2bec04f4f8f751157c78bb19579ac53d66717306
Tomas Vanagas <tomasvanagas@ymail.com>
2025-11-21 16:30:40 +0000

    Fixed control-backend APP_DEBUG variable bug

--- control-modules/control-backend/main.py
+++ control-modules/control-backend/main.py
@@ -7,8 +7,12 @@
 
 
 import sys
+import os
 from app import create_app
 
+
+# Environment variables
+APP_DEBUG = os.getenv('APP_DEBUG', 'false').lower() == "true"
 
 
 
@@ -16,15 +20,12 @@ from app import create_app
 
 if __name__ == '__main__':
     
-
     if(len(sys.argv) == 1):
         print("[*] Explanation is comming... ")
 
-
-
     elif(sys.argv[1] == "--http"):
         app = create_app()
-        app.run(host='0.0.0.0', port=8000, debug=True)
+        app.run(host='0.0.0.0', port=8000, debug=APP_DEBUG)
 
 
 
