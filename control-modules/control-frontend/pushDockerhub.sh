c03df0a21c0627cd8b42864a10b63e06af9aba1b
Tomas Vanagas <tomasvanagas@ymail.com>
2026-08-07 13:36:40 +0300

    Push docker hub for frontend

---
+++ control-modules/control-frontend/pushDockerhub.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -e
+
+CONTAINER_NAME=hosting-control-frontend
+
+TODAY=$(date +%Y%m%d)
+sudo docker login -u admin@knf.vu.lt
+sudo docker build -t $CONTAINER_NAME .
+sudo docker tag $CONTAINER_NAME vuknf/$CONTAINER_NAME:latest
+sudo docker tag $CONTAINER_NAME vuknf/$CONTAINER_NAME:$TODAY
+sudo docker push vuknf/$CONTAINER_NAME:latest
+sudo docker push vuknf/$CONTAINER_NAME:$TODAY
